6ed675a41f5d1e42aad0ef94f94538948b484f0f
Anshuman Praharaj <103830605+anshumancodes@users.noreply.github.com>
2022-12-03 13:42:41 +0530

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+let avgsoon=document.querySelector(".csm");
+avgsoon.addEventListener("click",(e)=>{
+    alert("resource will be available for download soon!")
+    e.preventDefault()
+    
+})
+// navbar mobile view toggle logic
+// let Mobilemenu = document.querySelector("#menu-bars");
+//         let nav = document.querySelector(".navbar");
+        
+//         // shrinkmenu.addEventListener('click',()=>{
+//         //     shrinkmenu.classList.toggle('fa-times')
+//         // })
+//         Mobilemenu.onclick = () => {
+//             Mobilemenu.classList.toggle("fa-circle-xmark");
+//             nav.classList.toggle("active")
+        
+//         };
+//         window.onscroll = () => {
+//             Mobilemenu.classList.remove("fa-circle-xmark");
+//             nav.classList.toggle("active")
+        
+//         };
